0593dd3d86888356e51e037fe056486357a72b39
VEETS5 <torinavito5@gmail.com>
2026-03-27 19:40:33 -0500

    feat: add nixcord (kinda)

--- home/nixcord.nix
+++ home/nixcord.nix
@@ -3,22 +3,17 @@
   programs.nixcord = {
     enable = true;
     vesktop.enable = true;
+
     config = {
       frameless = true;
       plugins = {
         hideAttachments.enable = true;
-        noTrack.enable = true;
-        clearURLs.enable = true;
-        fixCodeblockGap.enable = true;
-        gameActivityToggle.enable = true;
-        messageLogger.enable = true;
-        platformIndicators.enable = true;
-        reverseImageSearch.enable = true;
-        showHiddenChannels.enable = true;
-        showHiddenThings.enable = true;
-        betterRoleContext.enable = true;
-        memberCount.enable = true;
-        mentionAvatars.enable = true;
+        ignoreActivities = {
+          enable = true;
+          ignorePlaying = true;
+          ignoredActivities = [
+          ];
+        };
       };
     };
   };
